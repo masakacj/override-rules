7168df184db3da8f72992678457c99e74a225e9b
masakacj <34531511+masakacj@users.noreply.github.com>
2026-06-04 17:41:41 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -112,10 +112,8 @@ function main(config: ClashConfig): ClashConfig {
         frontProxySelector,
     });
 
-    const globalProxies = proxyGroups
-        .map((item) => String(item.name))
-        .filter((name) => name !== PROXY_GROUPS.SUCAI);
-    
+    const globalProxies = proxyGroups.map((item) => String(item.name));
+
     proxyGroups.push({
         name: PROXY_GROUPS.GLOBAL,
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Global.png`,
